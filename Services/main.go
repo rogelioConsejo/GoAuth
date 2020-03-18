9bbd79836041f5f93016f5c156376c346c0cbe0d
MeltingV4 <robertoserorojo@hotmail.com>
2020-03-18 17:53:30 -0600

    se creo la funcionalidad de creacion de servicios

---
+++ Services/main.go
@@ -0,0 +1,16 @@
+package main
+
+func main()  {
+	//carga automatica de servicios
+	ConfiguracionInicial()
+
+	//registro de servicios
+
+
+	//registro de nueva funcion en un servicio
+	NuevoServicio("\"NUEVO\":{\"s\":\"0\",\"s\":\"3\"}")
+
+	//exportacion de servicios
+	ExportarConfiguracion()
+
+}
